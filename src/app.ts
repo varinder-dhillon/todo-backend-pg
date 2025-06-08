324882fa0bae9309db4aa4d25303e5171e3769e1
Varinder <varinderdhilloninfo@gmail.com>
2025-06-08 15:44:37 +0530

    Setup (App): Added production ready setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 import express from "express";
-
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan";
 
 const app = express();
 
+// Global middleware
+
+// Security headers
+app.use(helmet());
+// Request logs
+if(process.env.NODE_ENV === 'development') app.use(morgan("dev"))
+
+// Request rate limitter
+const limitter = rateLimit({
+    windowMs: 1000*60*10,
+    limit: 100,
+    message: "Too many requests from this IP, Please try again in 10 Minutes!"
+})
+app.use("/api", limitter);
+
+// Routes
 
 
-
+// Global Error handler
 
 export default app;
