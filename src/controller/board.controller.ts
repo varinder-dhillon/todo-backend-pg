9d4ed6a617e33badd8b1b91b29af2ecb3035e698
Varinder <varinderdhilloninfo@gmail.com>
2025-06-15 15:09:07 +0530

    Feat (Board): Added the new API to retrieve the board data and its todods

--- src/controller/board.controller.ts
+++ src/controller/board.controller.ts
@@ -38,3 +38,59 @@ export const createBoard = catchAsync(async (req, res, next) => {
     })
 })
 
+export const getBoard = catchAsync(async (req, res, next) => {
+    const id = Number(req?.params?.id);
+
+    if(!id) next(new AppError("Board id is not present in the Req", status.fail)) 
+
+    // Simple query which will return the data in row base structure                NOTE: After query result also need map and format the data accordingly!
+    // const query = `
+    //     SELECT 
+    //         b.id boardId
+    //         b.name name
+    //     FROM board b
+    //     LEFT JOIN
+    //         todos t on b.id = t.boardId
+    //     Where b.id = $1;
+    // `;
+
+    // Advanced query which which will return the data in json formatter with aggregation functions
+    const queryWithAggr = `
+        SELECT 
+            b.id,
+            b.name,
+            b.description,
+            b.created_at,
+            b.updated_at,
+
+            COALESCE(
+                Json_agg(
+                    Json_build_object(
+                        'id', t.id,
+                        'name', t.name,
+                        'description', t.description,
+                        'status', t.status,
+                        'icon', t.icon,
+                        'boardId', t.boardId,
+                        'created_at', t.created_at,
+                        'updated_at', t.updated_at
+                    )
+                ) FILTER (WHERE t.id IS NOT NULL),
+                '[]'
+            ) as todos
+        FROM board b
+        LEFT JOIN
+            todos t on b.id = t.boardId
+        Where b.id = $1
+        GROUP BY b.id;
+    `;
+
+    const queryResult = await pool.query(queryWithAggr, [id])
+
+    res.status(status.success).json({
+        status: "Success",
+        message: queryResult.rows.length === 0 ? "No board found with the id!" : 'Retrieved the board data.',
+        data: queryResult.rows[0]
+    })
+
+})
--- src/routes/board.routes.ts
+++ src/routes/board.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { createBoard } from "../controller/board.controller";
+import { createBoard, getBoard } from "../controller/board.controller";
 
 
 const router = express.Router();
 
 router.post("/", createBoard)
+router.get("/:id", getBoard)
 
 
 export default router;
