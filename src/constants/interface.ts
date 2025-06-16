e89cf550da9082a71f7a4cafa23c45a01d17bd2c
Varinder <varinderdhilloninfo@gmail.com>
2025-06-16 23:39:44 +0530

    Feat (Zod): Added the ZOD Schema objects of payloads

--- src/constants/interface.ts
+++ src/constants/interface.ts
@@ -1,24 +1,40 @@
+import { z } from "zod";
 
-export interface IBoard extends Document {
-  name: string;
-  description: string;
-  createdAt?: Date;
-  updatedAt?: Date;
-}
 
-export interface ITask extends Document {
-  name: string;
-  description: string;
-  icon: string;
-  status: string;
-  boardId: Number;
-  createdAt?: Date;
-  updatedAt?: Date;
-}
+export const BoardSchema = z.object({
+    name: z.string().trim().min(1, "Name can't be empty!"),
+    description: z.string().trim().optional(),
+    created_at: z.date().optional(),
+    updated_at: z.date().optional(),
+})
 
-export interface Task {
-  name: string;
-  description: string;
-  icon: string;
-  status: string;
-} 
+export type IBoard = z.infer<typeof BoardSchema>;
+
+// export interface IBoard extends Document {
+//   name: string;
+//   description: string;
+//   createdAt?: Date;
+//   updatedAt?: Date;
+// }
+
+export const TaskSchema = z.object({
+    name: z.string().trim().min(1, "Name can't be empty!"),
+    description: z.string().trim(),
+    icon: z.string().trim(),
+    status: z.string().trim(),
+    boardId: z.number().min(1, 'Board Id is required!'),
+    created_at: z.date().optional(),
+    updated_at: z.date().optional(),
+});
+
+export type ITask  = z.infer<typeof TaskSchema>;
+
+// export interface ITask extends Document {
+//     name: string;
+//     description: string;
+//     icon: string;
+//     status: string;
+//     boardId: Number;
+//     createdAt?: Date;
+//     updatedAt?: Date;
+// }
